f7324187ecd838dd9a5d7b780063d4bf9cf761dd
János Pap <murmurian@gmail.com>
2022-11-19 16:39:43 +0200

    Created tokenizer

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,29 +5,112 @@ import java.util.Stack;
 
 public class Calculator {
     public static void main(String[] args) {
+
+        // TODO: Dependency injection for the scanner
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Enter an expression or quit with q: ");
             String expression = scanner.nextLine().replaceAll("\\s", "");
             if (expression.equals("q"))
                 break;
-            Queue<String> postfix = infixToPostfix(expression);
+            Queue<String> infix = tokenize(expression);
+
+            // TODO: Validate the infix expression
+
+            Queue<String> postfix = infixToPostfix(infix);
             System.out.println("The result is: " + evaluate(postfix));
         }
     }
 
     /**
+     * Tokenizes the given expression into a queue of Strings representing supported
+     * operators, operands and functions.
+     * 
+     * @param expression the expression to tokenize
+     * @return a queue of Strings representing the expression
+     */
+    public static Queue<String> tokenize(String expression) {
+        Queue<String> infix = new ArrayDeque<>();
+        String[] tokens = expression.split("(?<=[-+*/()^])|(?=[-+*/()^])");
+
+        for (String token : tokens) {
+            if (isOperator(token) || isParentheses(token) || isNumber(token)) {
+                infix.add(token);
+            } else if (token.length() > 2) {
+                infix.addAll(tokenizeFunction(token));
+            } else {
+                infix.add(token);
+            }
+        }
+        return infix;
+    }
+
+    /**
+     * Recurcisvely tokenizes a String into a queue of functions. Supports composite
+     * functions.
+     * 
+     * @param token the function to tokenize
+     * @return a queue of Strings representing the function
+     */
+    private static ArrayDeque<String> tokenizeFunction(String token) {
+        ArrayDeque<String> function = new ArrayDeque<>();
+        String functionToken = token.substring(0, 3);
+
+        if (isFunction(functionToken)) {
+            function.add(functionToken);
+            if (isNumber(token.substring(3)))
+                function.add(token.substring(3));
+            else if (token.substring(3).length() > 2)
+                function.addAll(tokenizeFunction(token.substring(3)));
+            else if (token.substring(3).length() != 0)
+                function.add(token.substring(3));
+        } else
+            function.add(token);
+
+        return function;
+    }
+
+    /**
+     * Checks if the given token is a parenthesis.
+     * 
+     * @param token the token to check
+     * @return true if the token is a parenthesis, false otherwise
+     */
+    private static boolean isParentheses(String token) {
+        return token.equals("(") || token.equals(")");
+    }
+
+    /**
+     * Checks if the given token is an operator.
+     * 
+     * @param token the token to check
+     * @return true if the token is an operator, false otherwise
+     */
+    private static boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/") || token.equals("^");
+    }
+
+    /**
+     * Checks if the given token is a supported function.
+     * 
+     * @param token the token to check
+     * @return true if the token is a function, false otherwise
+     */
+    private static boolean isFunction(String token) {
+        return token.equals("sin") || token.equals("cos") || token.equals("tan");
+    }
+
+    /**
      * Converts an infix expression to a postfix expression
+     * 
      * @param expression the infix expression
      * @return the postfix as a queue expression
      */
-    public static Queue<String> infixToPostfix(String expression) {
+    public static Queue<String> infixToPostfix(Queue<String> infix) {
         Queue<String> queue = new ArrayDeque<>();
         Stack<String> stack = new Stack<>();
 
-        String[] tokens = expression.split("(?<=[-+*/()])|(?=[-+*/()])");
-
-        for (String token : tokens) {
+        for (String token : infix) {
             switch (token) {
                 case "(" -> stack.push(token);
                 case ")" -> {
@@ -45,7 +128,17 @@ public class Calculator {
                         queue.add(stack.pop());
                     stack.push(token);
                 }
-                default -> queue.add(token);
+                case "^" -> {
+                    while (!stack.isEmpty() && stack.peek().equals("^"))
+                        queue.add(stack.pop());
+                    stack.push(token);
+                }
+                default -> {
+                    if (isFunction(token))
+                        stack.push(token);
+                    else
+                        queue.add(token);
+                }
             }
         }
 
@@ -57,17 +150,19 @@ public class Calculator {
 
     /**
      * Evaluates a postfix expression
+     * 
      * @param queue the postfix expression
      * @return the result
      */
     public static Double evaluate(Queue<String> queue) {
         Stack<Double> stack = new Stack<>();
 
+        // TODO: Implement strategy design pattern?
         while (!queue.isEmpty()) {
             String token = queue.remove();
             if (isNumber(token))
                 stack.push(Double.parseDouble(token));
-            else {
+            else if (isOperator(token)) {
                 double operand2 = stack.pop();
                 double operand1 = stack.pop();
                 switch (token) {
@@ -75,6 +170,14 @@ public class Calculator {
                     case "-" -> stack.push(operand1 - operand2);
                     case "*" -> stack.push(operand1 * operand2);
                     case "/" -> stack.push(operand1 / operand2);
+                    default -> stack.push(Math.pow(operand1, operand2));
+                }
+            } else if (isFunction(token)) {
+                double operand = stack.pop();
+                switch (token) {
+                    case "sin" -> stack.push(Math.sin(operand));
+                    case "cos" -> stack.push(Math.cos(operand));
+                    default -> stack.push(Math.tan(operand));
                 }
             }
         }
@@ -82,8 +185,13 @@ public class Calculator {
         return stack.pop();
     }
 
+    private static Double evaluateFunction(String token, double operand1) {
+        return null;
+    }
+
     /**
      * Checks if a token is a number
+     * 
      * @param token the string to check
      * @return true if the string is a number, false otherwise
      */
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,35 +9,79 @@ import java.util.Queue;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    private String expression1 = ("1+2");
+    private String expression2 = ("1.0+2*3.0");
+    private String expression3 = ("(1+2)*3");
+    private String trigExpression1 = ("sin(1)");
+    private String trigExpression2 = ("sin1.0+cos(2)");
+    private String trigExpression3 = ("sincos(2.3)*tanx3");
+
+    private Queue<String> infix1 = new ArrayDeque<>(Arrays.asList("1", "+", "2"));
+    private Queue<String> infix2 = new ArrayDeque<>(Arrays.asList("1.0", "+", "2", "*", "3.0"));
+    private Queue<String> infix3 = new ArrayDeque<>(Arrays.asList("(", "1", "+", "2", ")", "*", "3"));
+    private Queue<String> trigInfix1 = new ArrayDeque<>(Arrays.asList("sin", "(", "1", ")"));
+    private Queue<String> trigInfix2 = new ArrayDeque<>(Arrays.asList("sin", "1.0", "+", "cos", "(", "2", ")"));
+    private Queue<String> trigInfix3 = new ArrayDeque<>(Arrays.asList("sin", "cos", "(", "2.3", ")", "*", "tan", "x3"));
+
+    private Queue<String> postfix1 = new ArrayDeque<>(Arrays.asList("1", "2", "+"));
+    private Queue<String> postfix2 = new ArrayDeque<>(Arrays.asList("1.0", "2", "3.0", "*", "+"));
+    private Queue<String> postfix3 = new ArrayDeque<>(Arrays.asList("1", "2", "^", "3", "*"));
+
     @Test
     public void firstTest() {
         assertTrue(true);
     }
 
     @Test
-    public void infixToPostfixWorks() {
-        Queue<String> result = new ArrayDeque<>(Arrays.asList("1", "2", "+"));
-        assertIterableEquals(result, Calculator.infixToPostfix("1+2"));
-        result = new ArrayDeque<>(Arrays.asList("1", "2", "3", "*", "+"));
-        assertIterableEquals(result, Calculator.infixToPostfix("1+2*3"));
-        result = new ArrayDeque<>(Arrays.asList("1", "2", "*", "3", "+"));
-        assertIterableEquals(result, Calculator.infixToPostfix("(1*2)+3"));
+    public void testTokenizeBasicArithmetics() {
+        assertIterableEquals(infix1, Calculator.tokenize(expression1));
+        assertIterableEquals(infix2, Calculator.tokenize(expression2));
+        assertIterableEquals(infix3, Calculator.tokenize(expression3));
+    }
+
+    @Test
+    public void testTokenizeTrigFunctions() {
+        assertIterableEquals(trigInfix1, Calculator.tokenize(trigExpression1));
+        assertIterableEquals(trigInfix2, Calculator.tokenize(trigExpression2));
+        assertIterableEquals(trigInfix3, Calculator.tokenize(trigExpression3));
+    }
+
+    @Test
+    public void testInfixToPostfixBasic() {
+        assertIterableEquals(new ArrayDeque<>(Arrays.asList("1", "2", "+")), Calculator.infixToPostfix(infix1));
+        assertIterableEquals(new ArrayDeque<>(Arrays.asList("1.0", "2", "3.0", "*", "+")), Calculator.infixToPostfix(infix2));
+        assertIterableEquals(new ArrayDeque<>(Arrays.asList("1", "2", "+", "3", "*")), Calculator.infixToPostfix(infix3));
     }
     
     @Test
-    public void evaluateWorks() {
-        Queue<String> postfix = new ArrayDeque<>(Arrays.asList("1", "2", "+"));
-        assertTrue(Calculator.evaluate(postfix) == 3);
-        postfix = new ArrayDeque<>(Arrays.asList("1", "2", "3", "*", "+"));
-        assertTrue(Calculator.evaluate(postfix) == 7);
-        postfix = new ArrayDeque<>(Arrays.asList("1", "2", "*", "3", "+"));
-        assertTrue(Calculator.evaluate(postfix) == 5);
+    public void testInfixToPostfixTrig() {
+        assertIterableEquals(new ArrayDeque<>(Arrays.asList("1", "sin")), Calculator.infixToPostfix(trigInfix1));
+        assertIterableEquals(new ArrayDeque<>(Arrays.asList("1.0", "sin", "2", "cos", "+")), Calculator.infixToPostfix(trigInfix2));
+    }
+
+    @Test
+    public void testEvaluateBasic() {
+        assertTrue(Calculator.evaluate(postfix1) == 3);
+        assertTrue(Calculator.evaluate(postfix2) == 7);
+        assertTrue(Calculator.evaluate(postfix3) == 3);
+    }
+
+    @Test
+    public void evaluateFalse() {
+        assertFalse(Calculator.evaluate(postfix1) == 4);
+        assertFalse(Calculator.evaluate(postfix2) == 8);
+        assertFalse(Calculator.evaluate(new ArrayDeque<>(Arrays.asList("1", "sin"))) == 1337);
+    }
+
+    @Test void testEvaluateTrig() {
+        assertTrue(Calculator.evaluate(new ArrayDeque<>(Arrays.asList("1", "sin"))) == Math.sin(1));
+        assertTrue(Calculator.evaluate(new ArrayDeque<>(Arrays.asList("1.0", "sin", "2", "cos", "+"))) == Math.sin(1.0) + Math.cos(2));
     }
 
     @Test
     public void calculatorWorks() {
-        assertTrue(Calculator.evaluate(Calculator.infixToPostfix("1+2")) == 3);
-        assertTrue(Calculator.evaluate(Calculator.infixToPostfix("1+2*3")) == 7);
-        assertTrue(Calculator.evaluate(Calculator.infixToPostfix("(1+2)*3")) == 9);
+        assertTrue(Calculator.evaluate(Calculator.infixToPostfix(Calculator.tokenize(expression1))) == 3);
+        assertTrue(Calculator.evaluate(Calculator.infixToPostfix(Calculator.tokenize(expression2))) == 7);
+        assertTrue(Calculator.evaluate(Calculator.infixToPostfix(Calculator.tokenize(expression3))) == 9);
     }
 }
