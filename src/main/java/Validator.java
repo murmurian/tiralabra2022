112afb5fbc23fcd7141f89a9ad8a230cb0a5602a
János Pap <murmurian@gmail.com>
2022-12-14 19:16:14 +0200

    Defined valid variables

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -1,3 +1,22 @@
 public class Validator {
+    private String errorMessage;
+
+    public Validator() {
+        errorMessage = "";
+    }
+
+    public String validateAssignment(String[] parts) {
+        if (parts.length > 2) {
+            errorMessage = "You can only assign one variable at a time. ";
+        }
+        if (!validateVariable(parts[0])) {
+            errorMessage += "Variable name is invalid.";
+        }
+        return this.errorMessage;
+    }
+
+    public boolean validateVariable(String string) {
+        return string.matches("[a-zA-Z]{1,3}[0-9]{0,4}");
+    }
     
 }
